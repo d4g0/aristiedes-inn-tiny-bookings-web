b6ba37c850ba174d70e2be7cd316144ee0b22046
d4g0 <tocarralero@gmail.com>
2022-05-05 16:47:17 -0400

    + atuhQuery

--- composables/useQuery.js
+++ composables/useQuery.js
@@ -20,7 +20,57 @@ export function useQuery(query, variables = {}) {
         ).then(res => {
 
             // console.log(res);
-            
+
+            return res.json();
+
+        }).then(data => {
+
+            result.value = data;
+            loading.value = false;
+
+        }).catch(e => {
+            loading.value = false;
+            error.value = e;
+        })
+    }
+
+    function refetch() {
+        loading.value = true;
+        result.value = null;
+        error.value = null;
+        hit()
+    }
+    return {
+        loading,
+        result,
+        error,
+        refetch
+    }
+
+}
+
+export function useAuthQuery(query, variables = {}, token = '') {
+    var loading = ref(true);
+    var result = ref(null);
+    var error = ref(null)
+
+    hit();
+
+    function hit() {
+        fetch(
+            process.env.API_URL,
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token
+                },
+                body: JSON.stringify({ query, variables })
+            }
+        ).then(res => {
+
+            // console.log(res);
+
             return res.json();
 
         }).then(data => {
