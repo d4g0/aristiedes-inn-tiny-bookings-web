b1db6360da4432e240ac30a37b501a662d1f9ce2
d4g0 <tocarralero@gmail.com>
2022-05-06 00:07:40 -0400

    [fix] make vueuse stable

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,5 +46,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config) {
+      config.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      });
+    }
   }
 }
