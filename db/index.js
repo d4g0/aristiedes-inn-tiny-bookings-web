e8d9b2f6312d2a75388c545c70cfd267d58d3233
d4g0 <tocarralero@gmail.com>
2022-05-08 03:11:42 -0400

    new data def

--- db/index.js
+++ db/index.js
@@ -21,3 +21,12 @@ export const EVENTS = {
         FORM_SUBMITION_EVENT: "form-submition"
     }
 }
+
+export const API_ERRORS = {
+    USER_NOT_FOUND: 'NOT_FOUND_RECORD_ERROR'
+}
+
+
+export const TOAST_TYPES = {
+    error: 'error-toast'
+}
