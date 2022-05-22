04dd48247022fb2d5ec9c942d476f847ba472442
d4g0 <tocarralero@gmail.com>
2022-05-22 16:42:55 -0400

    social img correction

--- pages/index.vue
+++ pages/index.vue
@@ -93,7 +93,7 @@ export default {
         {
           hid: "og:image",
           property: "og:image",
-          content: `https://${DOMAIN}/img/social/social.jpg`,
+          content: `https://${DOMAIN}/img/social/aristides-social.jpg`,
         },
         // Twitter
         {
@@ -119,7 +119,7 @@ export default {
         {
           hid: "twitter:image",
           property: "twitter:image",
-          content: `https://${DOMAIN}/img/social/social.jpg`,
+          content: `https://${DOMAIN}/img/social/aristides-social.jpg`,
         },
       ],
     };
