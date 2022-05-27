5b6a445267ec7c65b4bac7707df6b292e2ad755e
d4g0 <tocarralero@gmail.com>
2022-05-27 03:42:14 -0400

    feature: edit hotels

--- pages/admin/hotels.vue
+++ pages/admin/hotels.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="frame-30">
+  <div class="frame-30 pb-4">
     <MainHeading text="Hoteles" class="mt-10" />
     <CreateHotelSection class="mt-[50px]" />
+    <EditHotelSection class="mt-[50px]"/>
   </div>
 </template>
 
 <script>
 import MainHeading from "~/components/admin/global/MainHeading.vue";
 import CreateHotelSection from "~/components/admin/hotels/create_hotel_sec/CreateHotelSection.vue";
+import EditHotelSection from '~/components/admin/hotels/edit_hotels_sec/EditHotelSection.vue';
 
 export default {
   layout: "admin",
@@ -17,6 +19,7 @@ export default {
     //
     MainHeading,
     CreateHotelSection,
+    EditHotelSection,
   },
 };
 </script>
